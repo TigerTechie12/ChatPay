10dbe0272c7f9b06e003ce94da1356a73dbd4cd4
Shreyash <shreyashiitr22@gmail.com>
2026-03-30 06:07:28 +0530

    wallet-wallet payment

--- user-app/backend/src/routes/p2p.ts
+++ user-app/backend/src/routes/p2p.ts
@@ -9,7 +9,28 @@ userPayRouter.post('/pay',authMiddleware,async(req,res)=>{
     const {phoneNumber,amount}=req.body
     //@ts-ignore
     const userId=req.userId
-   
-    
-    
+    const amountInPaise=amount*100
+   try{
+const userBalance= await prisma.balance.findUnique({where:{userId:userId}})
+const availableBalance=userBalance.amount-userBalance.locked
+if(amountInPaise>availableBalance){
+    return res.status(400).json({message:"Insufficient Balance to pay"})}
+
+    const ifRecipent=await prisma.user.findUnique({where:{number:phoneNumber}})
+if(ifRecipent){
+const wallet2wallettxn=await prisma.$transaction(async(txn:any)=>{
+    await txn.$queryRaw`SELECT * FROM "Balance" WHERE "userId"=${userId} FOR UPDATE `
+const userCurrentBalance=await txn.balance.update({where:{userId:userId},data:{amount:{decrement:{amountInPaise}}}})
+const recipentCurrentBalance=await txn.balance.update({where:{number:phoneNumber},data:{amount:{increment:{amountInPaise}}}})
+
+}
+)
+return res.status(200).json({message:"Payment Successful to the Recipents wallet",})
+
+}}
+
+
+
+catch(e:any){return res.status(400).json({message:e.message})
+}
 })
