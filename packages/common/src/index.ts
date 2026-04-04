af0129ab1e0368d94a6bc2c9c6228a5cde3b198b
Shreyash <shreyashiitr22@gmail.com>
2026-04-05 03:31:31 +0530

    fix

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -8,6 +8,20 @@ export const UserSchema=zod.object({
 })
 
 export const OnRampSchema=zod.object({
-    amount:zod.number(),
+    amount:zod.number().positive(),
     provider:zod.enum(["STRIPE"])
+})
+
+export const OffRampSchema=zod.object({})
+
+export const p2pWSchema=zod.object({
+    phoneNumber:zod.number().max(10),
+    amount:zod.number().positive()
+})
+export const p2pBSchema=zod.object({
+amount:zod.number().positive(),
+ provider:zod.string(),
+ accountNumber:zod.number().positive(),
+ifscCode:zod.string()
+
 })
