bc603ab4ce94e11fc24543f9d49ddba4371eda05
Shreyash <shreyashiitr22@gmail.com>
2026-04-09 05:23:08 +0530

    added rate limit middleware

---
+++ packages/middleware/src/rateLimitMiddleware.ts
@@ -0,0 +1,15 @@
+import { rateLimit } from "./lib/rateLimit";
+export async function rateLimiter(prefix:string,limit:number,windowinSec:number){
+return async function(req:any,res:any,next:any){
+const key=`${prefix}:${req.userId || req.ip} `
+const result=await rateLimit(key,limit,windowinSec)
+if(!result.allowed){
+    return res.status(429).json({message:"Too Many Requests",retryAfter:result.retryAfter})
+}
+res.set('X-RateLimit-Limit',String(result.remaining))
+next()
+}
+
+
+
+}
