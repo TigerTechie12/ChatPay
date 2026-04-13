9da5db7a0ad52be96ec11270c159df276865b2e2
Shreyash <shreyashiitr22@gmail.com>
2026-04-13 14:40:20 +0530

    added publisher

--- user-app/backend/src/routes/p2m.ts
+++ user-app/backend/src/routes/p2m.ts
@@ -4,12 +4,14 @@ import { PrismaClient } from "@prisma/client";
 import { authMiddleware } from "../../../../packages/middleware/src/middleware";
 const p2mRouter=Router()
 const prisma=new PrismaClient()
+import IOredis from 'ioredis'
 p2mRouter.use(express.json())
 p2mRouter.use(authMiddleware)
-
+const redis=new IOredis()
 p2mRouter.post('/api/transfer/merchant',async(req,res)=>{
 const {amount,merchantId,label}=req.body
 const userId=req.userId
+const channel=`channel-${merchantId}`
 try{const totalAmount=await prisma.balance.findUnique({where:{userId:userId},select:{amount:true}})
 const locked=await prisma.balance.findUnique({where:{userId:userId},select:{locked:true}})
 const availableBalance=totalAmount?.amount! - locked?.locked!
@@ -20,9 +22,14 @@ if(availableBalance<amount*100){return res.status(400).json({message:"Insufficie
     await txn.$queryRaw`SELECT * FROM "Balance" WHERE "userId"=${userId} FOR UPDATE `
 
  await txn.balance.update({where:{userId:userId},data:{amount:{decrement:{amount:amount*100}}}})
-await txn.merchantBalance.update({where:{merchantId:merchantId},data:{amount:{increment:{amount:amount*100}}}})
-res.status(200).json({message:"Payment Successful to the Merchant"})
-})}
+await txn.merchantBalance.update({where:{merchantId:merchantId},data:{amount:{increment:{amount:amount*100}}
+}
+})
+
+})
+await redis.publish(channel,JSON.stringify({message:`Payment of ${amount}rs has been received successfully`}))
+return res.status(200).json({message:"Payment Successful to the Merchant"})
+}
 catch(e:any){return res.status(400).json({message:e.message})
 
 }})
