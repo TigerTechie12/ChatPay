3e60ebd88acc2daf461fe305c4f007e37208f46a
Shreyash <shreyashiitr22@gmail.com>
2026-04-20 22:45:41 +0530

    cron-job updated

--- merchant-app/backend/src/cron/autoPayout.ts
+++ merchant-app/backend/src/cron/autoPayout.ts
@@ -7,39 +7,30 @@ import cron from 'node-cron'
 import { merchantWithdrawalQueue } from '../lib/queue';
 
 cron.schedule("0 2 */2 * *",async()=>{
-    const merchantIds=await prisma.
-redis.set(key,value,'EX',3600,'NX')
-try{const merchantBalance=await prisma.merchantBalance.findUnique({where:{merchantId:merchantId}})
-if(merchantBalance.balance==0){return}
-const balance=merchantBalance.balance
-const merchantDetails=await prisma.merchant.findUnique({where:{merchantId:merchantId}})
-const bankAccountNumber=merchantDetails.bankAccountNumber
-const bankIfscCode=merchantDetails.bankIfscCode
-const bankAccountName=merchantDetails.bankAccountName
+redis.set()
+try{
+const allMerchants=await prisma.merchant.findMany({where:{
+    balance:{not:0},
+    bankAccountNumber:{not:null},
+    bankIfscCode:{not:null},
+    offRampStatus:{notIn:['QUEUED','PROCESSING','FAILED','RETRYPENDING']}
+},
+select:{id:true}
+})
 
-const offRampTxn=await prisma.offRampTransaction.findUnique({where:{merchantId:merchantId}})
-if(offRampTxn.status==='QUEUED' || 'PROCESSING' || 'RETRYPENDING'){return}
-await prisma.$transaction(async(txn:any)=>{
-    await txn.$queryRaw`SELECT * FROM "MerchantBalance" WHERE "merchantId"=${merchantId} FOR UPDATE `
-await prisma.merchantBalance.update({where:{merchantId:merchantId},data:{
-    locked:{increment:{balance}}}
-})
-await prisma.offRampTransaction.create({
-status:'QUEUED',
-amount:balance,
-merchantId:merchantId,
-bankAccountName:bankAccountName,
-bankAccountNumber:bankAccountNumber,
-bankIfscCode:bankIfscCode,
-startedAt:Date.now()
-})
-await merchantWithdrawalQueue.add('offRampTxn',{offRampTxnId:offRampTxn.id},{removeOnComplete:true,removeOnFail:{age:24*3600},attempts:5,backoff:{type:'fixed',delay:10000}},)
-await merchantWithdrawalQueue.setGlobalRateLimit(1,1000)
+if(allMerchants.length===0){return }
+const allMerchantsId=allMerchants.map((m:any)=>{m.id})
+for(let i=0; i<allMerchantsId.length; i++){
+
+    await prisma.$transaction(async(tx:any)=>{
 
 })
 
 }
-catch(e){}
 
+
+
+}
+catch(e){}
 
 })
