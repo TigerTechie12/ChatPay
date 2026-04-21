70d3455412a1682566633759fe6179640877df92
Shreyash <shreyashiitr22@gmail.com>
2026-04-21 17:10:30 +0530

    changes

--- merchant-app/backend/src/cron/autoPayout.ts
+++ merchant-app/backend/src/cron/autoPayout.ts
@@ -15,22 +15,47 @@ const allMerchants=await prisma.merchant.findMany({where:{
     bankIfscCode:{not:null},
     offRampStatus:{notIn:['QUEUED','PROCESSING','FAILED','RETRYPENDING']}
 },
-select:{id:true}
+include:{
+    bankAccountNumber:true,
+    bankIfscCode:true,
+    bankAccountName:true,
+    id:true
+}
 })
+const allMerchantsId=allMerchants.map((m:any)=>{return m.id})
 
-if(allMerchants.length===0){return }
-const allMerchantsId=allMerchants.map((m:any)=>{m.id})
 for(let i=0; i<allMerchantsId.length; i++){
-
-    await prisma.$transaction(async(tx:any)=>{
-
+await prisma.$transaction(async(txn:any)=>{
+    const id=`${allMerchantsId[i]}`
+    await txn.$queryRaw`SELECT * FROM "MerchantBalance" WHERE "merchantId"=${allMerchantsId[i]} FOR UPDATE `
+    
+    const merchantDetails= await txn.merchantBalance.findUnique({where:`${allMerchantsId[i]}`})
+    
+    const increment=merchantDetails.amount                                             
+        const bankAccountName=merchantDetails.bankAccountName
+        const bankAccountNumber=merchantDetails.bankAccountNumber
+        const bankIfscCode=merchantDetails.bankIfscCode
+await txn.merchantBalance.update({where:`${allMerchantsId[i]}`,data:{
+    locked:{increment:{increment}}
+}})
+const offRampTransaction=await txn.offRampTransaction.create({
+amount:increment,
+accountNumber:bankAccountNumber,
+ifscCode:bankIfscCode,
+provider:bankAccountName,
+startedAt:Date.now(),
+merchantId:id
 })
 
+
+await merchantWithdrawalQueue.add('offRampTxn',{offRampTxnId:offRampTransaction.id},{removeOnComplete:true,removeOnFail:{age:24*3600},attempts:5,backoff:{type:'fixed',delay:10000}},)
+await merchantWithdrawalQueue.setGlobalRateLimit(1,1000)
+})
 }
 
 
-
 }
-catch(e){}
+catch(e){console.log(e)}
 
+finally{redis.del()}
 })
