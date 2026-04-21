e2affdd45a13c57ce304f5f1d840194af823ce42
Shreyash <shreyashiitr22@gmail.com>
2026-04-21 18:56:35 +0530

    fixes and changes

--- merchant-app/backend/src/cron/autoPayout.ts
+++ merchant-app/backend/src/cron/autoPayout.ts
@@ -1,61 +1,63 @@
 import express from 'express'
 import IOredis from 'ioredis'
 import { PrismaClient } from '@prisma/client';
-const prisma=PrismaClient()
+const prisma=new PrismaClient()
 const redis=new IOredis()
 import cron from 'node-cron'
 import { merchantWithdrawalQueue } from '../lib/queue';
 
 cron.schedule("0 2 */2 * *",async()=>{
-redis.set()
-try{
+try{const redisLock=await redis.set("payout-lock","running","EX",3600,"NX")
+if(redisLock===null){return}
 const allMerchants=await prisma.merchant.findMany({where:{
-    balance:{not:0},
+    
     bankAccountNumber:{not:null},
     bankIfscCode:{not:null},
-    offRampStatus:{notIn:['QUEUED','PROCESSING','FAILED','RETRYPENDING']}
-},
-include:{
-    bankAccountNumber:true,
-    bankIfscCode:true,
-    bankAccountName:true,
-    id:true
+    bankAccountName:{not:null}
+    
+},include:{
+    merchantBalance:true,
+    offRampTransaction:true
 }
 })
-const allMerchantsId=allMerchants.map((m:any)=>{return m.id})
 
-for(let i=0; i<allMerchantsId.length; i++){
-await prisma.$transaction(async(txn:any)=>{
-    const id=`${allMerchantsId[i]}`
-    await txn.$queryRaw`SELECT * FROM "MerchantBalance" WHERE "merchantId"=${allMerchantsId[i]} FOR UPDATE `
+
+for(let i=0; i<allMerchants.length; i++){
+let offRampTransaction:any
+    await prisma.$transaction(async(txn:any)=>{
     
-    const merchantDetails= await txn.merchantBalance.findUnique({where:`${allMerchantsId[i]}`})
+    const merchantId=allMerchants[i].merchantBalance.id
+    await txn.$queryRaw`SELECT * FROM "MerchantBalance" WHERE "merchantId"=${merchantId} FOR UPDATE `
     
+    const merchantDetails= await txn.merchantBalance.findUnique({where:{merchantId:merchantId}})
+    if(merchantDetails.amount ===0){return}
     const increment=merchantDetails.amount                                             
-        const bankAccountName=merchantDetails.bankAccountName
-        const bankAccountNumber=merchantDetails.bankAccountNumber
-        const bankIfscCode=merchantDetails.bankIfscCode
-await txn.merchantBalance.update({where:`${allMerchantsId[i]}`,data:{
-    locked:{increment:{increment}}
+        const bankAccountName=allMerchants[i].bankAccountName
+        const bankAccountNumber=allMerchants[i].bankAccountNumber
+        const bankIfscCode=allMerchants[i].bankIfscCode
+await txn.merchantBalance.update({where:{merchantId:merchantId},data:{
+    locked:{increment:increment}
 }})
 const offRampTransaction=await txn.offRampTransaction.create({
-amount:increment,
+data:{
+    amount:increment,
 accountNumber:bankAccountNumber,
 ifscCode:bankIfscCode,
 provider:bankAccountName,
-startedAt:Date.now(),
-merchantId:id
-})
+startedAt:new Date(),
+merchantId:merchantId,
+status:'QUEUED'
+}})
 
+})
 
 await merchantWithdrawalQueue.add('offRampTxn',{offRampTxnId:offRampTransaction.id},{removeOnComplete:true,removeOnFail:{age:24*3600},attempts:5,backoff:{type:'fixed',delay:10000}},)
-await merchantWithdrawalQueue.setGlobalRateLimit(1,1000)
-})
+
 }
 
 
 }
 catch(e){console.log(e)}
 
-finally{redis.del()}
+finally{redis.del("payout-lock")}
 })
